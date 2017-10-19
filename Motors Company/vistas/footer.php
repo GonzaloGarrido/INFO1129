ce7714b2615bd402a0fd59cdc2d04e163a04439e
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2017-10-19 09:52:53 -0300

    Estructuración MVC

--- Motors Company/vistas/footer.php
+++ Motors Company/vistas/footer.php
@@ -1,6 +1,7 @@
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript" src="style/css/bootstrap.min.js"></script>
-
+	<script type="text/javascript" src="style/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="style/js/bootstrap.js"></script>
+	<script type="text/javascript" src="style/js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="style/js/index.js"></script>
 </body>
 </html>
--- Motors Company/vistas/header.php
+++ Motors Company/vistas/header.php
@@ -1,31 +1,51 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>MVC</title>
-    <link rel="stylesheet" type="text/css" href="style/css/bootstrap.min.css">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  	<meta name="viewport" content="width=device-width, initial-scale=1"> <!--- Etiqueta responsividad -->
+	<title>Speed Cars Chile</title>
+	<link rel="hortcut Icon" href="style/img/favicon.ico" type="image/x-icon" />
+ 	<link rel="stylesheet" href="style/css/bootstrap.min.css">
+  	<link rel="stylesheet" href="style/css/bootstrap.css">
+  	<link rel="stylesheet" href="style/css/index.css">
 </head>
 <body>
-<header>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">MVC</a>
-          </div>
-          <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-              <li class="dropdown">
-              <li class="active"><a href="index.php?m=estudiante">Nuevo registro</a></li>
-                <a href="index.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Listar <span class="caret"></span></a>
-              </li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-      </nav>
-</header>
+	<div class="container">
+    <br>
+	    <header>
+	      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	        <div class="container-fluid">
+	          <div class="navbar-header">
+	            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="navbar-1">
+	              <span class="sr-only">Menu</span>
+	              <span class="icon-bar"></span>
+	              <span class="icon-bar"></span>
+	              <span class="icon-bar"></span>
+	            </button>
+	            <a href="index.php"><img src="style/img/logo.png" class="navbar-brand" width="90" height="70"></a>
+	            <a href="index.php" class="navbar-brand">Speed Cars Chile</a>
+	          </div>
+	          <div class="collapse navbar-collapse" id="navbar-1">
+	            <ul class="nav navbar-nav navbar-right">
+	              <li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Inicio</a></li>
+	              <li><a href=""><span class="glyphicon glyphicon-th-list"></span> Sucursales</a></li>
+	              <li><a href=""><span class="glyphicon glyphicon-user"></span> Nosotros</a></li>
+	              <li><a href=""><span class="glyphicon glyphicon-phone-alt"></span> Contacto</a></li>
+	              <li><a href=""><span class="glyphicon glyphicon-comment"></span> Ayuda</a></li>
+	              <li><a href=""><span class="glyphicon glyphicon-log-in"></span> Administrador</a></li>
+	              <!--- <li class="dropdown">
+	                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Sucursales <span class="caret"></span></a>
+	                <ul class="dropdown-menu">
+	                  <li><a href="">Angol</a></li>
+	                  <li><a href="">Temuco</a></li>
+	                  <li><a href="">Victoria</a></li>
+	                </ul> 
+	              </li> -->
+	            </ul>
+	          </div>
+	        </div>
+	      </nav>
+	    </header>
+  	</div>
