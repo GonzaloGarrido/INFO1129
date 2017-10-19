2de8c6f9a127fbb044650e935ae9eea600341576
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2017-10-19 09:26:15 -0300

    Aplicando MVC Corregido

--- Motors Company/index.php
+++ Motors Company/index.php
@@ -1,21 +1,57 @@
-<?php
-    require_once('bd/conexion.php');
-    require_once('controlador/estudiante_controller.php');
-
-    $controller= new estudiante_controller();
-    
-    if(!empty($_REQUEST['m'])){
-        $metodo=$_REQUEST['m'];
-        if (method_exists($controller, $metodo)) {
-            $controller->$metodo();
-        }else{
-            $controller->index();
-        }
-    }else{
-        $controller->index();
-    }
-
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1"> <!--- Etiqueta responsividad -->
+	<title>Speed Cars Chile</title>
+	<link rel="hortcut Icon" href="img/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/index.css">
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/index.js"></script>
+</head>
+<body>
+	<div class="container">
+		<br>
+		<header>
+			<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="navbar-1">
+							<span class="sr-only">Menu</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a href="index.html"><img src="img/logo.png" class="navbar-brand" width="90" height="70"></a>
+						<a href="index.html" class="navbar-brand">Speed Cars Chile</a>
+					</div>
+					<div class="collapse navbar-collapse" id="navbar-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="index.html"><span class="glyphicon glyphicon-home"></span> Inicio</a></li>
+							<li><a href=""><span class="glyphicon glyphicon-th-list"></span> Sucursales</a></li>
+							<li><a href=""><span class="glyphicon glyphicon-user"></span> Nosotros</a></li>
+							<li><a href=""><span class="glyphicon glyphicon-phone-alt"></span> Contacto</a></li>
+							<li><a href=""><span class="glyphicon glyphicon-comment"></span> Ayuda</a></li>
+							<li><a href=""><span class="glyphicon glyphicon-log-in"></span> Ingresar</a></li>
+							<!--- <li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Sucursales <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="">Angol</a></li>
+									<li><a href="">Temuco</a></li>
+									<li><a href="">Victoria</a></li>
+								</ul> 
+							</li> -->
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</header>
+	</div>
+</body>
+</html>
