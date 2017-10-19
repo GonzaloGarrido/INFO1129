8556d3ebdb79ffde86cc931ff925c0ccb382e97c
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2017-10-19 09:35:21 -0300

    Modificado index

--- Motors Company/index.php
+++ Motors Company/index.php
@@ -6,14 +6,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<meta name="viewport" content="width=device-width, initial-scale=1"> <!--- Etiqueta responsividad -->
 	<title>Speed Cars Chile</title>
-	<link rel="hortcut Icon" href="img/favicon.ico" type="image/x-icon" />
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="css/index.css">
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.js"></script>
-	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-	<script type="text/javascript" src="js/index.js"></script>
+	<link rel="hortcut Icon" href="style/img/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" href="style/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style/css/bootstrap.css">
+	<link rel="stylesheet" href="style/css/index.css">
+	<script type="text/javascript" src="style/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="style/js/bootstrap.js"></script>
+	<script type="text/javascript" src="style/js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="style/js/index.js"></script>
 </head>
 <body>
 	<div class="container">
@@ -28,17 +28,17 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<a href="index.html"><img src="img/logo.png" class="navbar-brand" width="90" height="70"></a>
-						<a href="index.html" class="navbar-brand">Speed Cars Chile</a>
+						<a href="index.php"><img src="style/img/logo.png" class="navbar-brand" width="90" height="70"></a>
+						<a href="index.php" class="navbar-brand">Speed Cars Chile</a>
 					</div>
 					<div class="collapse navbar-collapse" id="navbar-1">
 						<ul class="nav navbar-nav navbar-right">
-							<li><a href="index.html"><span class="glyphicon glyphicon-home"></span> Inicio</a></li>
+							<li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Inicio</a></li>
 							<li><a href=""><span class="glyphicon glyphicon-th-list"></span> Sucursales</a></li>
 							<li><a href=""><span class="glyphicon glyphicon-user"></span> Nosotros</a></li>
 							<li><a href=""><span class="glyphicon glyphicon-phone-alt"></span> Contacto</a></li>
 							<li><a href=""><span class="glyphicon glyphicon-comment"></span> Ayuda</a></li>
-							<li><a href=""><span class="glyphicon glyphicon-log-in"></span> Ingresar</a></li>
+							<li><a href=""><span class="glyphicon glyphicon-log-in"></span> Administrador</a></li>
 							<!--- <li class="dropdown">
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Sucursales <span class="caret"></span></a>
 								<ul class="dropdown-menu">
