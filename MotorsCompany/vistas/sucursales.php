3da7fadf396eec75cbbae57bd0785c763d24e975
GonzaloGarrido <gonzalo.garrido2016@alu.uct.cl>
2017-10-29 17:03:36 -0300

    Maqueta de sucursales
    solo maqueteo por ahora solo es introducir los datos necesarios y listo :)

---
+++ MotorsCompany/vistas/sucursales.php
@@ -0,0 +1,35 @@
+
+<div class="container" style="margin:40px auto;">
+	<div class="row" style="width: 80%;margin: 50px auto;">
+		<div class="col-md-4 col-xs-4 col-sm-4" style="margin: 30px auto;">
+			<div style="height: 150px; margin: 5px auto;background:white; border-radius: 10px;border:solid 1px black;"><h1>IMAGEN1</h1></div>
+			<div style="height: 300px; margin:5px auto; background: white; border-radius: 10px;border:solid 1px black;">
+				<h1>TXT descripcion sucursal</h1>
+			</div>
+			<div style="margin:5px auto; background: white; border-radius: 10px;border:solid 1px black;">
+				<center><h1>boton</h1></center>
+			</div>
+			
+		</div>
+		<div class="col-md-4 col-xs-4 col-sm-4" style="margin: 30px auto;">
+			<div style="height: 150px; margin: 5px auto;background:white; border-radius: 10px;border:solid 1px black;"><h1>IMAGEN1</h1></div>
+			<div style="height: 300px; margin:5px auto; background: white; border-radius: 10px;border:solid 1px black;">
+				<h1>TXT descripcion sucursal</h1>
+			</div>
+			<div style="margin:5px auto; background: white; border-radius: 10px;border:solid 1px black;">
+				<center><h1>boton</h1></center>
+			</div>
+			
+		</div>
+		<div class="col-md-4 col-xs-4 col-sm-4" style="margin: 30px auto;">
+			<div style="height: 150px; margin: 5px auto;background:white; border-radius: 10px;border:solid 1px black;"><h1>IMAGEN1</h1></div>
+			<div style="height: 300px; margin:5px auto; background: white; border-radius: 10px;border:solid 1px black;">
+				<h1>TXT descripcion sucursal</h1>
+			</div>
+			<div style="margin:5px auto; background: white; border-radius: 10px;border:solid 1px black;">
+				<center><h1>boton</h1></center>
+			</div>
+			
+		</div>
+	</div>
+</div>
