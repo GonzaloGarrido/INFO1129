96676f9fe579d4592e4db2497be8907b6fd2bfc4
GonzaloGarrido <gonzalo.garrido2016@alu.uct.cl>
2017-11-26 23:47:18 -0300

    final
    adkasjkldjasd final :)

--- MotorsCompany/vistas/sucursales.php
+++ MotorsCompany/vistas/sucursales.php
@@ -1,32 +1,38 @@
 <div class="container" style="margin:40px auto;">
 	<div class="row" style="width: 80%;margin: 50px auto;">
 		<div class="col-md-4 col-xs-4 col-sm-4" style="margin: 30px auto;">
-			<div style="height: 150px; margin: 5px auto;background:white; border-radius: 10px;border:solid 1px black;"><h1>IMAGEN1</h1></div>
+			<div style="height: 150px; margin: 5px auto;background:white; border-radius: 10px;border:solid 1px black;"><img src="img/angol.jpg" style="width: 271px;height: 148px;border-radius: 10px;"></div>
 			<div style="height: 300px; margin:5px auto; background: white; border-radius: 10px;border:solid 1px black;">
-				<h1>TXT descripcion sucursal</h1>
+				<h4>A nuestros queridos clientes. SpeedCarsChile es una empresa familiar cuya creación hace 9 años es el resultado del exito. Sobre las principales ofertas del rubro automotriz existentes en Angol y la región.</h4>
 			</div>
-			<div style="margin:5px auto; background: white; border-radius: 10px;border:solid 1px black;">
-				<center><h1>boton</h1></center>
+			<div style="margin:5px auto; background: white; border-radius: 10px;">
+				<center>
+					<input type="submit" name="sucrsal1" value="Angol" style="margin: 5px;width: 200px;height: 60px;border-radius: 10px;">
+				</center>
 			</div>
 			
 		</div>
 		<div class="col-md-4 col-xs-4 col-sm-4" style="margin: 30px auto;">
-			<div style="height: 150px; margin: 5px auto;background:white; border-radius: 10px;border:solid 1px black;"><h1>IMAGEN1</h1></div>
+			<div style="height: 150px; margin: 5px auto;background:white; border-radius: 10px;border:solid 1px black;"><img src="img/temuco.jpg" style="width: 271px;height: 150px;border-radius: 10px;"></div>
 			<div style="height: 300px; margin:5px auto; background: white; border-radius: 10px;border:solid 1px black;">
-				<h1>TXT descripcion sucursal</h1>
+				<h4>A nuestros queridos clientes. SpeedCarsChile es una empresa familiar cuya creación hace 9 años es el resultado del exito. Sobre las principales ofertas del rubro automotriz existentes en Temuco y la región.</h4>
 			</div>
-			<div style="margin:5px auto; background: white; border-radius: 10px;border:solid 1px black;">
-				<center><h1>boton</h1></center>
+			<div style="margin:5px auto; background: white; border-radius: 10px;">
+				<center>
+					<input type="submit" name="sucrsal2" value="Temuco" style="margin: 5px;width: 200px;height: 60px;border-radius: 10px;">
+				</center>
 			</div>
 			
 		</div>
 		<div class="col-md-4 col-xs-4 col-sm-4" style="margin: 30px auto;">
-			<div style="height: 150px; margin: 5px auto;background:white; border-radius: 10px;border:solid 1px black;"><h1>IMAGEN1</h1></div>
+			<div style="height: 150px; margin: 5px auto;background:white; border-radius: 10px;border:solid 1px black;"><img src="img/victoria.jpg" style="width: 275px;height: 150px;border-radius: 10px;"></div>
 			<div style="height: 300px; margin:5px auto; background: white; border-radius: 10px;border:solid 1px black;">
-				<h1>TXT descripcion sucursal</h1>
+				<h4>A nuestros queridos clientes. SpeedCarsChile es una empresa familiar cuya creación hace 9 años es el resultado del exito. Sobre las principales ofertas del rubro automotriz existentes en Victoria y la región.</h4>
 			</div>
-			<div style="margin:5px auto; background: white; border-radius: 10px;border:solid 1px black;">
-				<center><h1>boton</h1></center>
+			<div style="margin:5px auto; background: white; border-radius: 10px;">
+				<center>
+					<input type="submit" name="sucrsal3" value="Victoria" style="margin: 5px;width: 200px;height: 60px;border-radius: 10px;">
+				</center>
 			</div>
 			
 		</div>
